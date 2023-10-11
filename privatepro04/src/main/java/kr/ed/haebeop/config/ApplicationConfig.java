38fd181f63eb8c9f84e526d2e2f4ef651b5df3d4
seogwang <seogwang7779@gmail.com>
2023-10-11 17:49:27 +0900

    Build : Join, Login and User Detail

--- privatepro04/src/main/java/kr/ed/haebeop/config/ApplicationConfig.java
+++ privatepro04/src/main/java/kr/ed/haebeop/config/ApplicationConfig.java
@@ -1,19 +1,11 @@
 package kr.ed.haebeop.config;
 
-import kr.ed.haebeop.repository.TestRepository;
-import kr.ed.haebeop.repository.TestRepositoryImpl;
-import kr.ed.haebeop.service.TestService;
-import kr.ed.haebeop.service.TestServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @ComponentScan(basePackages = "kr.ed.haebeop")
 public class ApplicationConfig {
-    @Bean
-    public TestService testService(){ return new TestServiceImpl(); }
-
-    @Bean
-    public TestRepository testRepository() { return new TestRepositoryImpl(); }
 }
---
+++ privatepro04/src/main/java/kr/ed/haebeop/controller/UserController.java
@@ -0,0 +1,120 @@
+package kr.ed.haebeop.controller;
+
+import kr.ed.haebeop.domain.User;
+import kr.ed.haebeop.service.UserService;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    HttpSession session;
+
+    @RequestMapping(value = "getUser", method = RequestMethod.GET)
+    public String getUser(Model model, HttpServletRequest request) throws Exception {
+//      String id = (String) request.getParameter("sid"); 테스트용
+        String id = (String) session.getAttribute("sid");
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        return  "/user/getUser";
+    }
+
+    @GetMapping("agree")
+    public String getAgree(Model model) throws Exception {
+        return "/user/agree";
+    }
+
+    @GetMapping("join")
+    public String userJoin(Model model) throws Exception {
+        return "user/join";
+    }
+
+    @RequestMapping(value = "idCheck", method = RequestMethod.POST)
+    public void idCheck(HttpServletResponse response, HttpServletRequest request, Model model) throws Exception {
+        String id = request.getParameter("id");
+        boolean result = false;
+        User dto = new User();
+        dto = userService.getUser(id);
+        if(dto!=null) {
+            result = false;
+        } else {
+            result = true;
+        }
+        JSONObject json = new JSONObject();
+        json.put("result", result);
+        PrintWriter out = response.getWriter();
+        out.println(json.toString());
+    }
+
+    @RequestMapping(value = "insert",method = RequestMethod.POST)
+    public String userWrite(User user, Model model) throws Exception {
+        //비밀번호 암호화
+        String userpw = user.getPw();
+        String pwd = passwordEncoder.encode(userpw);
+        user.setPw(pwd);
+        userService.userInsert(user);;
+        return "user/loginForm";
+    }
+
+    @RequestMapping("loginForm")
+    public String loginForm(Model model) throws Exception {
+        return "user/loginForm";
+    }
+
+    @RequestMapping(value="signin", method = RequestMethod.POST)
+    public String userSignin(@RequestParam String id, @RequestParam String pw, HttpServletRequest req, RedirectAttributes rttr) throws Exception {
+        session.invalidate();
+        User mdto = new User();
+        mdto.setPw(pw);
+        mdto.setId(id);
+        User login = userService.signIn(mdto);
+        boolean loginSuccess = passwordEncoder.matches(mdto.getPw(), login.getPw());
+        if (loginSuccess && login != null) {
+            session.setAttribute("user", login);
+            session.setAttribute("sid", id);
+            return "redirect:/";
+        } else {
+            return "redirect:loginForm";
+        }
+    }
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String userLogin(User mdto, HttpServletRequest req, RedirectAttributes rttr) throws Exception {
+        boolean loginSuccess = userService.logIn(req);
+        if(loginSuccess) {
+            return "redirect:/";
+        } else {
+            return "redirect:loginForm";
+        }
+    }
+
+//    @RequestMapping(value = "loginCheck.do", method = RequestMethod.POST)
+//    public String userAjaxLogin(User mdto, RedirectAttributes rttr) throws Exception {
+//        session.getAttribute("user");
+//        User user = userService.loginCheck(mdto);
+//        boolean mat = passwordEncoder.matches(mdto.getPw(), user.getPw());
+//        if(mat==true && user!=null) {
+//
+//        }
+//    }
+}
